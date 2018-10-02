09284079878e60286b679075230943b99b809c3a
Gaetan Garcon <gaetangarcon@Gaetans-MacBook-Air.local>
2018-10-02 13:47:49 -0400

    Initial commit

---
+++ src/main/java/com/example/demo/Song.java
@@ -0,0 +1,54 @@
+package com.example.demo;
+
+public class Song {
+    private long id;
+    private  String id;
+    private  String artist;
+    private String album;
+    private int rating;
+    private int year;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public void setArtist(String artist) {
+        this.artist = artist;
+    }
+
+    public String getAlbum() {
+        return album;
+    }
+
+    public void setAlbum(String album) {
+        this.album = album;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+}
